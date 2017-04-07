e13a8ced7328cf3f8dff6f71380503768a91c3ce
wukuai <symtywy@163.com>
2017-04-07 23:23:29 +0800

    Update 23Merge k Sorted Lists.cpp

--- 23Merge k Sorted Lists.cpp
+++ 23Merge k Sorted Lists.cpp
@@ -1,12 +1,4 @@
-//方法一，一个总的，依次两两合并，时间复杂度O(n*k^2)，链表平均长度为n，k个链表
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+//方法一，一个总的，依次两两合并，时间复杂度O(n*k^2)，链表平均长度为n，k个链表,406ms。。。。
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
@@ -37,3 +29,20 @@ public:
         return temp;
     }
 };
+
+//优化，29ms，平均分摊合并，分治，时间复杂度？
+ListNode* mergeKLists(vector<ListNode*>& lists) {
+    int n=lists.n();
+    if(n==0) return NULL;
+    int i=0,j=n-1;
+    while(n!=1){
+        lists[i]=mergeTwoLists(lists[i],lists[j]);
+        i++,j--;
+        if(i>=j){//注意当n为奇数的时候=也可以
+            if(n%2==0) n=n/2;
+            else n=n/2+1;//注意这里，当原来n为奇数的时候，别忘了+1；多想例子，如n=3 and n=4
+            i=0,j=n-1;
+        }
+    }
+    return lists[0];
+}
