6e5d56e091d414b9c050ff35507e8f5ba0a61ace
wukuai <symtywy@163.com>
2017-04-20 22:57:18 +0800

    判断链表是否有环~while(p1&&p2&&p2->next&&p1!=p2)

--- 141Linked List Cycle.cpp
+++ 141Linked List Cycle.cpp
@@ -1,5 +1,6 @@
 //判断链表是否有环
 //while(p1&&p2&&p2->next&&p1!=p2)
+//while(p1!=NULL&&p2!=NULL&&p2->next!=NULL&&p1!=p2)
 //https://leetcode.com/problems/linked-list-cycle
 /**
  * Definition for singly-linked list.
@@ -14,7 +15,7 @@ public:
     bool hasCycle(ListNode *head) {
         if(head==NULL||head->next==NULL) return false;
         ListNode *p1=head;
-        ListNode *p2=head->next;
+        ListNode *p2=head->next->next;
         while(p1&&p2&&p2->next&&p1!=p2){
         //while(p1!=NULL&&p2!=NULL&&p2->next!=NULL&&p1!=p2){ 
         //make sure that p2->next!=NULL is important,because we will use p2->next->next
