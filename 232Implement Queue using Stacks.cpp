a28fac6fc9bfea9d8bd625f2e4b3bf5dc58b6706
wukuai <symtywy@163.com>
2017-04-22 00:24:21 +0800

    两个栈模拟队列~push只管stack1, top/pop只管stack2

--- 232Implement Queue using Stacks.cpp
+++ 232Implement Queue using Stacks.cpp
@@ -1,6 +1,9 @@
 //用两个栈模拟队列
+//关键点在于：.top() or .pop() 时只需考虑stack2的顶部元素（它一定是最先进来的）,如果stack2为空，则倒腾一遍
+//.push()时只需push到stack1中即可，不用管stack2
+//则：常规情况下，push只管stack1   top/pop只管stack2
+//这就避免了每次倒腾过来又倒腾过去了
 //https://leetcode.com/problems/implement-queue-using-stacks
-//待改进版：
 class MyQueue {
 public:
     /** Initialize your data structure here. */
@@ -15,44 +18,37 @@ public:
     
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        if(stack1.empty()) return -1;
-        int temp;
-        while(!stack1.empty()){
-            temp=stack1.top();
-            stack1.pop();
-            stack2.push(temp);
+        if(stack1.empty()&&stack2.empty()) return -1;
+        if(stack2.empty()){
+            int temp;
+            while(!stack1.empty()){
+                temp=stack1.top();
+                stack1.pop();
+                stack2.push(temp);
+            }
         }
         int top=stack2.top();
         stack2.pop();
-        while(!stack2.empty()){
-            temp=stack2.top();
-            stack2.pop();
-            stack1.push(temp);
-        }
         return top;
     }
     
     /** Get the front element. */
     int peek() {
-        if(stack1.empty()) return -1;
-        int temp;
-        while(!stack1.empty()) {
-            temp=stack1.top();
-            stack1.pop();
-            stack2.push(temp);
+        if(empty()) return -1;
+        if(stack2.empty()){
+            int temp;
+            while(!stack1.empty()){
+                temp=stack1.top();
+                stack1.pop();
+                stack2.push(temp);
+            }
         }
-        int top=stack2.top();
-        while(!stack2.empty()) {
-            temp=stack2.top();
-            stack2.pop();
-            stack1.push(temp);
-        }
-        return top;
+        return stack2.top();
     }
     
     /** Returns whether the queue is empty. */
     bool empty() {
-        return stack1.empty()?true:false;
+        return stack1.empty()&&stack2.empty();
     }
 private:
     stack<int>stack1;
